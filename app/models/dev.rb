071c1f70538eeaf2ac08ded6eb5d96ecd0e6c062
Walker Le <walker.le88@gmail.com>
2023-08-16 21:52:09 -0600

    cc complete

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,25 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
+
+  def received_one? item_name
+    freebie_items = self.freebies.map {|freebie| freebie.item_name}
+    freebie_items.include? item_name
+
+    # Alternate Solution
+    # self.freebies.any? {|freebie| freebie.item_name == item_name}
+  end
+
+  def give_away dev, freebie
+    if self.received_one? freebie.item_name
+      freebie.update(dev: dev)
+      "#{freebie.item_name} now belongs to #{dev.name}"
+    else
+      "#{freebie.item_name} doesn't belong to #{self.name}"
+    end
+
+    # Alternate Solution
+    # freebie.update(dev: dev) unless freebie.dev != self
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+puts "Destroying old data..."
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -17,5 +22,11 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+# Freebie.create(item_name: "", value: , company_id: , dev_id: )
+Freebie.create(item_name: "Pen", value: 2, company_id: 1, dev_id: 2)
+Freebie.create(item_name: "Backpack", value: 30, company_id: 3, dev_id: 4)
+Freebie.create(item_name: "Notepad", value: 5, company_id: 1, dev_id: 1)
+Freebie.create(item_name: "Water Bottle", value: 8, company_id: 2, dev_id: 3)
+Freebie.create(item_name: "Flashlight", value: 12, company_id: 4, dev_id: 1)
 
 puts "Seeding done!"
